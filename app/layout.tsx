587daca2bee124a85f077e33aa5c7800b897d4ae
ashkan <ashkanpc2003@gmail.com>
2023-11-28 14:22:03 +0330

    setup prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // npm i react-icons
 // npm i classnames //A simple JavaScript utility for conditionally joining classNames together.
-
+// npm i prisma@5.3.1 // npx prisma init for the first amount
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";

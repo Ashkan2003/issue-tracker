ce0ce32f81eb402f03e786b9a8bbd2141c7c4e0e
ashkan <ashkanpc2003@gmail.com>
2023-12-02 10:53:52 +0330

    create issues table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table, TableHeader } from "@radix-ui/themes";
 import Link from "next/link";
-const IssuesPage = () => {
+import prisma from "@/prisma/client";
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany(); // get all the issues from the db// const issues is an array of obj
+  console.log(issues, "dddd");
   return (
     <div>
-      <Button><Link href="/issues/new">New Issue</Link></Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+      {/* variant="surface" is a design value for creating a beutifull table */}
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };

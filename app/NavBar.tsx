c33c860edb99f6a73bf5eb000a25d7fd9596e1a6
ashkan <ashkanpc2003@gmail.com>
2023-12-13 17:07:08 +0330

    navbar ui change

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames"; // we use classname-package to see witch classes are rendered in witch condition
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 const NavBar = () => {
   const currentPath = usePathname(); //Get the current pathname
   const { status, data: session } = useSession();
@@ -16,36 +16,41 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b px-5 h-14 mb-5 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.lable}>
-            <Link
-              className={classNames({
-                // "class-name" : true or fulse //true= the class will always rendered // false= the condithon if is false then the claass does not applyes
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.lable}
+    <nav className="border-b px-5  mb-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Log in</Link>
-        )}
-        
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.lable}>
+                  <Link
+                    className={classNames({
+                      // "class-name" : true or fulse //true= the class will always rendered // false= the condithon if is false then the claass does not applyes
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.lable}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Log out</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Log in</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };

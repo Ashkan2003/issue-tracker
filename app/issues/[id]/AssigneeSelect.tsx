f2318ebe847264e4c891c9ec8829b1c9d3d09904
ashkan <ashkanpc2003@gmail.com>
2023-12-24 18:31:51 +0330

    fetch data with react-query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,20 +2,28 @@
 
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]); // the type "User[]" is a array of obj
 
-  // in this useEffect-hook we fetch the users from db by axios and set it to the users-state
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users"); // we only want to fetch the data in type of <User[]>
-      setUsers(data); // we set the data(an array of abj) to the user
-    };
-    fetchUsers();
-  }, []);
+  // we used react-query to cash the users
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"], // the queryKey is a unic key to identify the data in the cash
+    queryFn: () => axios.get("/api/users").then((res) => res.data), // we pass a function to this to fetch the data
+    staleTime: 60 * 1000, //60s // the time to refetch the data
+    retry:3, // if fetching data was not succes, then try 3 more times
+  });
+
+  if(isLoading) return <Skeleton height="2rem"/>;
+
+  if(error) return null
 
   return (
     <Select.Root>
@@ -23,7 +31,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>

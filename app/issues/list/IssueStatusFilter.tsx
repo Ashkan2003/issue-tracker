7bba5dfe7477f0dbc19cd59b49c9a04bd50813d1
ashkan <ashkanpc2003@gmail.com>
2023-12-26 18:32:58 +0330

    fix some bug related to sorting and filtering

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -14,10 +14,19 @@ const statuses: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  console.log(searchParams, "fff");
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : "";
+        //
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        //
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
     >
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -49,7 +49,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell key={column.value} className={column.className}>
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },

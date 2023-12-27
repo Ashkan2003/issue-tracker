e7ccfa2fd0b58dbd26dbfbc1678ad78c40c77707
ashkan <ashkanpc2003@gmail.com>
2023-12-27 12:43:08 +0330

    refactor list-page

---
+++ app/issues/list/IssueTable.tsx
@@ -0,0 +1,71 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { Table } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+import NextLink from "next/link";
+import { Issue, Status } from "@prisma/client";
+
+interface Props {
+  // the "status" and "orderby" and "page" are the searchPrams query-strings
+  searchParams: { status: Status; orderBy: keyof Issue; page: string };
+  issues: Issue[];
+}
+
+const columns: { label: string; value: keyof Issue; className?: string }[] = [
+  // the columns is a array of obj // the type of "value" is the keys of Issue-obj // the className-property is optional
+  { label: "Issue", value: "title" },
+  { label: "Status", value: "status", className: "hidden md:table-cell" },
+  { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+];
+
+export const columnNames = columns.map((column) => column.value);
+
+const IssueTable = ({ searchParams, issues }: Props) => {
+  return (
+    // variant="surface" is a design value for creating a beutifull table
+    <Table.Root variant="surface">
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell
+              key={column.value}
+              className={column.className}
+            >
+              <NextLink
+                href={{
+                  query: { ...searchParams, orderBy: column.value },
+                }}
+              >
+                {column.label}
+              </NextLink>
+              {column.value === searchParams.orderBy && (
+                <ArrowUpIcon className="inline" />
+              )}
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map((issue) => (
+          <Table.Row key={issue.id}>
+            <Table.Cell>
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <div className="block md:hidden">
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              <IssueStatusBadge status={issue.status} />
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              {issue.createdAt.toDateString()}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+export default IssueTable;
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,14 +1,10 @@
 // this page has the logic of implementing filtering and sorthing
 
 import prisma from "@/prisma/client";
-import { Table } from "@radix-ui/themes";
-import IssueStatusBadge from "../../components/IssueStatusBadge";
-import Link from "../../components/Link";
-import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
-import NextLink from "next/link";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
 import Pagination from "../_components/Pagination";
+import IssueActions from "./IssueActions";
+import IssueTable, { columnNames } from "./IssueTable";
 
 interface Props {
   // the "status" and "orderby" and "page" are the searchPrams query-strings
@@ -16,14 +12,6 @@ interface Props {
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  //
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    // the columns is a array of obj // the type of "value" is the keys of Issue-obj // the className-property is optional
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
-
   //filtering section // we want to validate the search-params.its not nessesery but if a user type a serach-param it self in the url then we get an error
   const statuses = Object.values(Status); //statuses is an array of 3-values =>["OPEN","IN_PROGRESS","CLOSED"] //Object.value Returns an array of values of the enumerable properties of an object
   const status = statuses.includes(searchParams.status)
@@ -31,9 +19,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
     : undefined; // if the searchParams.status was not in the statuses-array put undefind in the status // prisma will render all of the issues if the "where"-value was undefind
 
   //sorthing section// we want to validate the search-params.its not nessesery but if a user type a serach-param it self in the url then we get an error
-  const orderby = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const orderby = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
@@ -52,50 +38,9 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const issueCount = await prisma.issue.count({ where: { status: status } });
 
   return (
-    <div>
+    <div className="space-y-5">
       <IssueActions />
-      {/* variant="surface" is a design value for creating a beutifull table */}
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.className}
-              >
-                <NextLink
-                  href={{
-                    query: { ...searchParams, orderBy: column.value },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {column.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issues} />
       <Pagination
         pageSize={pageSize}
         currentPage={page}

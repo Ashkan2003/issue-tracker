052547ce44507d68779f47ecbfb12ed8f24e7129
ashkan <ashkanpc2003@gmail.com>
2023-12-27 10:49:32 +0330

    implementing pagination

--- app/issues/_components/Pagination.tsx
+++ app/issues/_components/Pagination.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -5,6 +6,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -14,9 +16,22 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
-  const pageCount = Math.ceil(itemCount / pageSize); // the total numver of the pages
+  // if we only use userRouter,when we push the other seach-params like "status" and "orderBy" will be deleted
+  // so privoisly we new useSearchParams() to get the search-params
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  //calc the total numver of the pages
+  const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null; // if there is only one page so we dont need pagination
 
+  //we want to add the currentPage as a  query-string to the url
+  const changePage = (page: number) => {
+    // so get the cuurentPage as a arg
+    const params = new URLSearchParams(searchParams); //the params have methods, not search-strings // the URLSearchParams provide many methods to edit search-paramets
+    params.set("page", page.toString()); //this returns like page=2// the params.set method firs arg gets the key of search-params and the second arg gets the value of the search-params
+    router.push("?" + params.toString()); //this returns like "?page=2"// push this to the url
+  };
 
   // disabled={currentPage === 1} // when we are in the first-page, disable this btn to prevent an error
   // disabled={currentPage === pageCount} // when we are in the last-page, disable this btn to prevent an error
@@ -25,16 +40,36 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Pagination from "./issues/_components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
       <div>hellowk word</div>
-      <Pagination itemCount={100} pageSize={10} currentPage={2}/>
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
     </>
   );
 }

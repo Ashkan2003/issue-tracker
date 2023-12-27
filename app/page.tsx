baa0f30010e924c504750f51b04d63454f0f99de
ashkan <ashkanpc2003@gmail.com>
2023-12-27 19:27:21 +0330

    refactor the page-layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
+import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
-import Pagination from "./issues/_components/Pagination";
 import prisma from "@/prisma/client";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-  const inProgress = await prisma.issue.count({where: { status: "IN_PROGRESS" }});
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
 
   return (
-    // <LatestIssues/>
-    <IssueChart open={open} inProgress={inProgress} closed={closed} />
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+        <IssueChart open={open} closed={closed} inProgress={inProgress} />
+      </Flex>
+      <LatestIssues/>
+    </Grid>
   );
 }
